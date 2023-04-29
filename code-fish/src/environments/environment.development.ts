d8ff92496e5128493e216394ae4b4dd01c958ea5
Norbert Sári <nsa@licquid.com>
2023-04-29 20:19:14 +0200

    Revert "Delete code-fish/src/environments directory"
    This reverts commit 6a00ca5ab603916ed7cfbc8e236c7e9fe03da0d6.

---
+++ code-fish/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'codefish-df1fd',
+    appId: '1:218996978853:web:bcf84814abb592e72e094c',
+    storageBucket: 'codefish-df1fd.appspot.com',
+    apiKey: 'AIzaSyDrkMadleHtP9Z05VzrbJyR1Xx3N4qJNbw',
+    authDomain: 'codefish-df1fd.firebaseapp.com',
+    messagingSenderId: '218996978853',
+    measurementId: 'G-3XH8WF87K6',
+  },};
